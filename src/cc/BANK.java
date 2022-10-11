16d15a18b4f95e51bba1988fd488415c6bf68d76
ChiodaPietro <chioda.pietro@studenti.salesianisesto.it>
2022-10-11 09:22:03 +0200

    .

---
+++ src/cc/BANK.java
@@ -0,0 +1,9 @@
+package cc;
+
+public class BANK {
+    private float tot_money;
+    private String name, surname;
+    private int code_number;
+}
+
+
---
+++ src/cc/CC.java
@@ -0,0 +1,7 @@
+package cc;
+
+public class CC {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
